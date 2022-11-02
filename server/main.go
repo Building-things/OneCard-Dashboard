62f3ba31df5eb6a112219689741da71f9e6958f5
Rowan Apps <rwn757@gmail.com>
2022-11-01 19:37:27 -0700

    changed port to so heroku can bind

--- server/main.go
+++ server/main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	onecardparsing "onecard-dashboard/onecard-parsing"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
 	app := fiber.New()
 
 	app.Static("/", "./dist")
 
 	app.Post("/", Root)
 
-	app.Listen(":5000")
+	app.Listen(":" + port)
 }
 
 func Root(c *fiber.Ctx) error {
