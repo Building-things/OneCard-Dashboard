9c9094d9f9ff3d40c3695ac3524e2ce0a7ccfaca
LeoDeRosa <60721870+LeoDeRosa@users.noreply.github.com>
2023-03-01 23:12:33 -0800

    chart is in!

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,23 @@
+const res_amt_str = document.querySelectorAll("section.summary.transactions > ul > li > .transaction-amt")[0].innerHTML
+const res_amt_f = parseFloat(res_amt_str.substring(1).replace(",", ""))
+const starting_amt = 1291.75
+const current_date = new Date()
+const current_month = new Date().getMonth()
+const current_year = new Date().getFullYear()
+let term_end_date;
+if(current_month > 6){
+    term_end_date = new Date(`December 20, ${current_year} 23:59:000`)
+}else if(current_month < 5){
+    term_end_date = new Date(`April 29, ${current_year} 23:59:000`)
+}else{
+    alert("Summer Term Not Supported");
+}
+const time_between = term_end_date.getTime() - current_date.getTime();
+const time_between_days = Math.floor(time_between / (1000 * 60 * 60 * 24));
+const daily_budget = (res_amt_f / time_between_days).toFixed(2);
+
+
+
 function load_styles(){
     const head = document.getElementsByTagName('HEAD')[0];
     
@@ -20,7 +40,13 @@ function load_styles(){
 
     head.appendChild(link);
     head.appendChild(link2);
+
+    script.onload = function() {
+      drawRadialChart();
+    }
+
     head.appendChild(script);
+
 }
 
 function remove_elements(){
@@ -32,56 +58,47 @@ function remove_elements(){
 function add_elements(){
     
     const chartContainer = document.createElement("section")
-    chartContainer.innerText = "CHART HERE"
-    chartContainer.id = "radialChart"
-
-    var options = {
-        series: [70],
-        chart: {
-        height: 350,
-        type: 'radialBar',
-      },
-      plotOptions: {
-        radialBar: {
-          hollow: {
-            size: '70%',
-          }
-        },
-      },
-      labels: ['Cricket'],
-    };
-
-    var chart = new ApexCharts(document.querySelector("radialChart"), options);
-    chart.render();
-
+    chartContainer.id = "#chart"
+    
     document.querySelector("div.column-left").appendChild(chartContainer)
 }
 
 function spending_per_day(){
-    const res_amt_str = document.querySelectorAll("section.summary.transactions > ul > li > .transaction-amt")[0].innerHTML
-    const res_amt_f = parseFloat(res_amt_str.substring(1).replace(",", ""))
-    const current_date = new Date()
-    const current_month = new Date().getMonth()
-    const current_year = new Date().getFullYear()
-    let term_end_date;
-    if(current_month > 6){
-        term_end_date = new Date(`December 20, ${current_year} 23:59:000`)
-    }else if(current_month < 5){
-        term_end_date = new Date(`April 29, ${current_year} 23:59:000`)
-    }else{
-        alert("Summer Term Not Supported");
-    }
-    const time_between = term_end_date.getTime() - current_date.getTime();
-    const time_between_days = Math.floor(time_between / (1000 * 60 * 60 * 24));
-    const daily_budget = (res_amt_f / time_between_days).toFixed(2);
 
 
     const spending_node = document.createElement("p")
-    spending_node.innerHTML = `You can spend {daily_budget} per day.`
+    spending_node.innerHTML = `You can spend $${daily_budget} per day.`
     document.querySelector("div.column-right > section").prepend(spending_node)
 }
 
-load_styles()
+function drawRadialChart() {
+   
+  var options = {
+    series: [(res_amt_f / starting_amt) * 100],
+    chart: {
+    height: 450,
+    type: 'radialBar',
+  },
+  plotOptions: {
+    radialBar: {
+      hollow: {
+        size: '70%',
+      }
+    },
+  },
+  labels: ['Remaining'],
+  };
+  
+  var chart = new ApexCharts(document.getElementById("#chart"), options);
+  chart.render();
+
+}
+
+
+
 remove_elements()
-add_elements()
 spending_per_day()
+add_elements()
+load_styles()
+
+
